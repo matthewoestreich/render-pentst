66dba431a00823f72b2ef8761e50efc031e824db
Matt Oestreich <matthewpoestreich@gmail.com>
2025-02-15 15:48:00 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
+const nodePath = require("node:path");
 const express = require("express");
 const app = express();
 
 app.get("/rtchat/csrf", (req, res) => {
-	res.send(`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <script>
-    const ws = new WebSocket("wss://rtchat-a7ul.onrender.com/");
-    ws.onopen(event => {
-      console.log("opened!", {event});
-    });
-    ws.onmessage(event => {
-      console.log("onmessage", {event});
-    });
-  </script>
-</body>
-</html>`);
+  res.sendFile(nodePath.resolve(__dirname, "./rtchat.csrf.html"));
 });
 
 app.get("*", (req, res) => {
