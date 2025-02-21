f58ba746dfbc5681ea73e0ddd31c88be4c88cafb
Matt Oestreich <matthewpoestreich@gmail.com>
2025-02-21 08:34:14 -0600

    tst

--- index.js
+++ index.js
@@ -2,8 +2,19 @@ const nodePath = require("node:path");
 const express = require("express");
 const app = express();
 
-app.get("/rtchat/csrf", (req, res) => {
-  res.sendFile(nodePath.resolve(__dirname, "./rtchat.csrf.html"));
+const RTCHAT_URL = "https://rtchat-a7ul.onrender.com/"
+
+app.get("/rtchat/websocket-csrf", (req, res) => {
+  res.sendFile(nodePath.resolve(__dirname, "./rtchat.websocket.csrf.html"));
+});
+
+app.get("/rtchat/csrf", async (req, res) => {
+  const result = await fetch(RTCHAT_URL);
+  if (!result.ok) {
+    return res.send(result);
+  }
+  const body = await result.json();
+  res.send(body);
 });
 
 app.get("*", (req, res) => {
