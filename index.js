8f4c002d601b11f362e7ca2f0a0c5096008ced08
Matt Oestreich <matthewpoestreich@gmail.com>
2025-04-11 15:26:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,12 @@ const app = express();
 
 const RTCHAT_URL = "https://rtchat-a7ul.onrender.com/"
 
-app.use(express.static(nodePath.resolve(__dirname, "./public")));
+// For Wizer Challenge #48
+/* * */app.use(express.static(nodePath.resolve(__dirname, "./public")));
+/* * */app.get("/", (req, res) => {
+/* * */  res.send(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body><script src="/script.js"></script></body></html>    `);
+/* * */});
+// END for Wizer Challenge 48
 
 app.get("/rtchat/websocket-csrf", (req, res) => {
   res.sendFile(nodePath.resolve(__dirname, "./rtchat.websocket.csrf.html"));
@@ -13,24 +18,6 @@ app.get("/rtchat/websocket-csrf", (req, res) => {
 app.get("/rtchat/csrf", async (req, res) => {
   res.sendFile(nodePath.resolve(__dirname, "./rtchat.csrf.html"));
 });
-
-// For Wizer Challenge #48
-app.get("/challenge48", (req, res) => {
-  res.send(`
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-</head>
-<body>
-  <script src="/script.js"></script>
-</body>
-</html>    
-`);
-});
-// END for Wizer Challenge 48
 
 app.get("*", (req, res) => {
   res.status(404).send("not found");
