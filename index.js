e42249a85c7dd97e7bab775dc5dabcc289a61397
Matt Oestreich <matthewpoestreich@gmail.com>
2025-04-11 13:25:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.get("/script.js", (req, res) => {
   res.setHeader("Content-Type", "application/javascript");
   res.send("alert('Wizer');");
 });
+app.get("/test-script-js", (req, res) => {
+  res.send(`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <script src="/script.js" />
+</body>
+</html>    
+`);
+});
 
 app.get("*", (req, res) => {
   res.status(404).send("not found");
