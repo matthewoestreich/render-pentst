070360bca0f22779559642f40ab6d139ecdad933
Matt Oestreich <matthewpoestreich@gmail.com>
2025-04-11 15:22:22 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,14 +1,1 @@
-document.onload = () => {
-  alert("Wizer");
-  doAlert();
-}
-
-console.log("Loaded");
-
-function doAlert() {
-  alert("Wizer2");
-}
-
-window.addEventListener("load", () => {
-  alert("loaded");
-});
+alert("Wizer");
